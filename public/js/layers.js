79bf5d9ffd2efb2e2137b5bac53ee47d73abb2be
ramonacus <ramonacus@gmail.com>
2018-03-17 16:31:04 +0100

    At chapter 5, 29:04, just created the Matrix class

--- public/js/layers.js
+++ public/js/layers.js
@@ -1,10 +1,12 @@
-export function createBackgroundLayer(backgrounds, sprites) {
+export function createBackgroundLayer(level, sprites) {
     const buffer = document.createElement('canvas');
+    const bufferCtx = buffer.getContext('2d');
     buffer.width = 256;
     buffer.height = 240;
 
-    backgrounds.forEach(background => {
-        drawBackground(background, buffer.getContext('2d'), sprites);
+
+    level.tiles.forEach((tile, x, y) => {
+        sprites.drawTile(tile.name, bufferCtx, x, y);
     });
 
     return function drawBackgroundLayer(context) {
@@ -20,12 +22,3 @@ export function createSpriteLayer(entities) {
     }
 }
 
-function drawBackground(background, context, sprites) {
-    background.ranges.forEach(([x1, x2, y1, y2]) => {
-        for (let x = x1; x < x2; ++x) {
-            for (let y = y1; y < y2; ++y) {
-                sprites.drawTile(background.tile, context, x, y);
-            }
-        }
-    });
-}
