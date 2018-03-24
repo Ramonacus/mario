7cd6b1d432f06eb9086590f5cedaf28639d3b53d
ramon <ramonacus@gmail.com>
2018-03-24 11:52:55 +0100

    Lesson 6 up to minute 30:00, approximately

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -1,48 +1,91 @@
 import {createBackgroundLayer, createSpriteLayer} from "./layers.js";
 import Level from './Level.js';
-import {loadBackgroundSprites} from "./sprites.js";
+import Spritesheet from "./Spritesheet.js";
+
+function loadJSON(url) {
+	return fetch(url).then(data => data.json())
+}
 
 export function loadImage(url) {
-    return new Promise(resolve => {
-        const image = new Image();
-        image.addEventListener('load', () => {
-            resolve(image);
-        });
-        image.src = url;
-    });
+	return new Promise(resolve => {
+		const image = new Image();
+		image.addEventListener('load', () => {
+			resolve(image);
+		});
+		image.src = url;
+	});
 }
 
 function createTiles(level, backgrounds) {
-    backgrounds.forEach(background => {
-        background.ranges.forEach(([x1, x2, y1, y2]) => {
-            for (let x = x1; x < x2; ++x) {
-                for (let y = y1; y < y2; ++y) {
-                    level.tiles.set(x, y, {
-                        name: background.tile
-                    });
-                }
-            }
-        });
-    });
+	function applyRange(background, xStart, xLen, yStart, yLen) {
+		const xEnd = xStart + xLen;
+		const yEnd = yStart + yLen;
+
+		for (let x = xStart; x < xEnd; ++x) {
+			for (let y = yStart; y < yEnd; ++y) {
+				level.tiles.set(x, y, {
+					name: background.tile
+				});
+			}
+		}
+	}
+
+	backgrounds.forEach(background => {
+		background.ranges.forEach(range => {
+			if (range.length === 4) {
+				const [xStart, xLen, yStart, yLen] = range;
+				applyRange(background, xStart, xLen, yStart, yLen);
+			} else if (range.length === 3) {
+				const [xStart, xLen, yStart] = range;
+				applyRange(background, xStart, xLen, yStart, 1);
+			} else if (range.length === 2) {
+				const [xStart, yStart] = range;
+				applyRange(background, xStart, 1, yStart, 1);
+			}
+		});
+	});
 }
 
+function loadSpriteSheet(name) {
+	return loadJSON(`/sprites/${name}.json`).then(sheetSpec => Promise.all([
+		sheetSpec,
+		loadImage(sheetSpec.imageURL)
+	]))
+		.then(([sheetSpec, image]) => {
+			const sprites = new Spritesheet(
+				image,
+				sheetSpec.tileW,
+				sheetSpec.tileH);
+
+			sheetSpec.tiles.forEach(tileSpec => {
+				sprites.defineTile(
+					tileSpec.name,
+					tileSpec.index[0],
+					tileSpec.index[1]);
+			});
+
+			return sprites;
+		});
+}
+
+
 export function loadLevel(name) {
-    return Promise.all([
-        fetch(`/levels/${name}.json`).then(level => level.json()),
-        loadBackgroundSprites()
-    ])
-        .then(([levelSpec, backgroundSprites]) => {
-            const level = new Level();
+	return Promise.all([
+		loadJSON(`/levels/${name}.json`),
+		loadSpriteSheet('overworld')
+	])
+		.then(([levelSpec, backgroundSprites]) => {
+			const level = new Level();
 
-            createTiles(level, levelSpec.backgrounds);
+			createTiles(level, levelSpec.backgrounds);
 
-            const backgroundLayer = createBackgroundLayer(level, backgroundSprites);
-            level.comp.layers.push(backgroundLayer);
+			const backgroundLayer = createBackgroundLayer(level, backgroundSprites);
+			level.comp.layers.push(backgroundLayer);
 
-            const spriteLayer = createSpriteLayer(level.entities);
-            level.comp.layers.push(spriteLayer);
+			const spriteLayer = createSpriteLayer(level.entities);
+			level.comp.layers.push(spriteLayer);
 
-            return level;
+			return level;
 
-        });
+		});
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,46 +1,43 @@
 import Timer from "./Timer.js";
+import Camera from "./Camera.js";
 import {loadLevel} from "./loaders.js";
 import {createMario} from "./entities.js";
 import {createCollisionLayer} from "./layers.js";
 import {setupKeyboard} from "./input.js";
+import {setupMouseControl} from "./debug.js";
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 
 Promise.all([
-    createMario(),
-    loadLevel('1-1')
+	createMario(),
+	loadLevel('1-1')
 ])
-    .then(([mario, level]) => {
+	.then(([mario, level]) => {
 
-        mario.pos.set(64, 64);
+		const camera = new Camera();
+		window.camera = camera;
+		mario.pos.set(64, 64);
 
-        level.comp.layers.push(createCollisionLayer(level));
+		level.comp.layers.push(createCollisionLayer(level));
 
-        level.entities.add(mario);
+		level.entities.add(mario);
 
-        const input = setupKeyboard(mario);
-        input.listenTo(window);
+		const input = setupKeyboard(mario);
+		input.listenTo(window);
 
-        ['mousedown', 'mousemove'].forEach(eventName => {
-            canvas.addEventListener(eventName, event => {
-                if (event.buttons === 1) {
-                    mario.vel.set(0, 0);
-                    mario.pos.set(event.offsetX, event.offsetY);
-                }
-            });
-        });
+		setupMouseControl(canvas, mario, camera );
 
-        const timer = new Timer(1 / 60);
+		const timer = new Timer(1 / 60);
 
-        timer.update = function update(deltaTime) {
-            level.update(deltaTime);
-            level.comp.draw(context);
-        };
+		timer.update = function update(deltaTime) {
+			level.update(deltaTime);
+			level.comp.draw(context, camera);
+		};
 
-        timer.start();
+		timer.start();
 
-    });
+	});
 
 
