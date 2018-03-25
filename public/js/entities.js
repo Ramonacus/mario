a02e2e8e4cfea473e09371823ce49577b1cfe001
ramon <ramonacus@gmail.com>
2018-03-25 12:31:55 +0200

    Lesson seven up to 14:30

--- public/js/entities.js
+++ public/js/entities.js
@@ -1,22 +1,32 @@
 import Entity from "./Entity.js";
-import {loadMarioSprite} from "./sprites.js";
-import Velocity from './traits/Velocity.js';
 import Jump from './traits/Jump.js';
 import Go from "./traits/Go.js";
+import {loadSpriteSheet} from "./loaders.js";
 
 export function createMario() {
-    return loadMarioSprite()
+    return loadSpriteSheet('mario')
         .then(sprite => {
-
             const mario = new Entity();
             mario.size.set(14, 16);
 
             mario.addTrait(new Jump());
             mario.addTrait(new Go());
-            //mario.addTrait(new Velocity());
+
+            const frames = ['run-1', 'run-2', 'run-3'];
+
+            function routeFrame(mario) {
+            	if (mario.go.dir !== 0) {
+					const correctionFactor = 10
+					const frameIndex = (Math.floor(mario.go.distance / correctionFactor) % frames.length)
+            		const frameName = frames[frameIndex];
+					return frameName;
+				}
+
+            	return 'idle';
+			}
 
             mario.draw = function drawMario(context) {
-                sprite.draw('idle', context, 0, 0);
+                sprite.draw(routeFrame(mario), context, 0, 0);
             };
 
             return mario;
--- public/js/traits/Go.js
+++ public/js/traits/Go.js
@@ -1,13 +1,22 @@
 import {Trait} from "../Entity.js";
 
-export default class Go extends Trait{
-    constructor() {
-        super('go');
-        this.dir = 0;
-        this.speed = 6000;
-    }
+export default class Go extends Trait {
+	constructor() {
+		super('go');
+		this.dir = 0;
+		this.speed = 6000;
 
-    update(entity, deltaTime) {
-        entity.vel.x = this.speed * this.dir * deltaTime;
-    }
+		this.distance = 0;
+	}
+
+	update(entity, deltaTime) {
+		entity.vel.x = this.speed * this.dir * deltaTime;
+
+		if (this.dir) {
+			this.distance += Math.abs(entity.vel.x) * deltaTime;
+		}
+		else {
+			this.distance = 0;
+		}
+	}
 }
